a3793edaf8eaf06216223d8ab1e778626e913895
Fernando Yepes <yepesjuanfer7532@gmail.com>
2022-09-17 13:11:52 -0500

    tabla periodica funcional

---
+++ src/index.js
@@ -0,0 +1,163 @@
+const API = 'https://neelpatel05.pythonanywhere.com'
+
+const root = document.querySelector('.root');
+
+const lanthanoids = document.createElement('div'); 
+lanthanoids.classList.add('lant');
+
+const gas = document.createElement('div');
+gas.classList.add('gas');
+
+const halogen = document.createElement('div'); 
+halogen.classList.add('halog');
+
+const alkali = document.createElement('div');
+alkali.classList.add('alkaliM');
+
+const earth = document.createElement('div');
+earth.classList.add('earth');
+
+
+const data = async () => {
+
+    const res = await fetch(API);
+    const data = await res.json();
+
+            
+    for (let i = 0; i < data.length; i++) {
+
+    let symbol = data[i].symbol;
+    let atomicNumber = data[i].atomicNumber;
+    let groupBlock = data[i].groupBlock;
+    let group = groupBlock.replace(/ /g, '-');
+
+    if (group != 'lanthanoid' && 
+        group != 'actinoid' && 
+        group != 'noble-gas' &&
+        group != 'halogen' &&
+        group != 'alkali-metal' &&
+        group != 'alkaline-earth-metal'
+        ) {
+        
+        const elementContainer = document.createElement('div');
+        elementContainer.classList.add('element', `element${atomicNumber}`, `${group}`);
+        elementContainer.addEventListener('click', () => {
+
+            console.log(`${symbol}, ${atomicNumber}`)
+        })
+
+        const number = document.createElement('p');
+        number.classList.add('atomicNumber');
+        number.textContent = atomicNumber;
+
+        const symbolElem = document.createElement('p');
+        symbolElem.classList.add('symbol');
+        symbolElem.textContent = symbol;
+
+        elementContainer.append(number, symbolElem);
+        root.append(elementContainer);
+    }
+
+    if (group === 'lanthanoid' || group === 'actinoid') {
+        
+
+        const elementContainer = document.createElement('div');
+        elementContainer.classList.add('element', `element${atomicNumber}`, `${group}`);
+
+        const number = document.createElement('p');
+        number.classList.add('atomicNumber');
+        number.textContent = atomicNumber;
+
+        const symbolElem = document.createElement('p');
+        symbolElem.classList.add('symbol');
+        symbolElem.textContent = symbol;
+
+        lanthanoids.append(elementContainer)
+        elementContainer.append(number, symbolElem);
+        root.append(lanthanoids);
+    }
+
+    if (group === 'noble-gas') {
+        
+
+        const elementContainer = document.createElement('div');
+        elementContainer.classList.add('element', `element${atomicNumber}`, `${group}`);
+
+        const number = document.createElement('p');
+        number.classList.add('atomicNumber');
+        number.textContent = atomicNumber;
+
+        const symbolElem = document.createElement('p');
+        symbolElem.classList.add('symbol');
+        symbolElem.textContent = symbol;
+
+        gas.append(elementContainer)
+        elementContainer.append(number, symbolElem);
+        root.append(gas);
+    }
+
+    if (group === 'halogen') {
+
+        const elementContainer = document.createElement('div');
+        elementContainer.classList.add('element', `element${atomicNumber}`, `${group}`);
+
+        const number = document.createElement('p');
+        number.classList.add('atomicNumber');
+        number.textContent = atomicNumber;
+
+        const symbolElem = document.createElement('p');
+        symbolElem.classList.add('symbol');
+        symbolElem.textContent = symbol;
+
+        halogen.append(elementContainer)
+        elementContainer.append(number, symbolElem);
+        root.append(halogen);
+        
+    }
+
+    if (group === 'alkali-metal') {
+
+
+        const elementContainer = document.createElement('div');
+        elementContainer.classList.add('element', `element${atomicNumber}`, `${group}`);
+
+        const number = document.createElement('p');
+        number.classList.add('atomicNumber');
+        number.textContent = atomicNumber;
+
+        const symbolElem = document.createElement('p');
+        symbolElem.classList.add('symbol');
+        symbolElem.textContent = symbol;
+
+        alkali.append(elementContainer)
+        elementContainer.append(number, symbolElem);
+        root.append(alkali);
+    }
+
+
+    if (group === 'alkaline-earth-metal') {
+
+
+        const elementContainer = document.createElement('div');
+        elementContainer.classList.add('element', `element${atomicNumber}`, `${group}`);
+
+        const number = document.createElement('p');
+        number.classList.add('atomicNumber');
+        number.textContent = atomicNumber;
+
+        const symbolElem = document.createElement('p');
+        symbolElem.classList.add('symbol');
+        symbolElem.textContent = symbol;
+
+        earth.append(elementContainer)
+        elementContainer.append(number, symbolElem);
+        root.append(earth);
+    }
+
+    }
+
+    console.log(data)
+    
+    }
+
+data()
